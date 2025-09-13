754eb7f30fe3f521219934f739f8090be4ba9a53
sengokyu <sengokyu+gh@gmail.com>
2025-09-13 15:35:50 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+export { createBugyoCloudClient } from "./bugyo-cloud-client-factory";
 export { BugyoCloudClient, BugyoCloudClientError } from "./bugyo-cloud-client";
-export { AuthInfo } from "./models/auth-info";
-export { ClockType, PunchInfo } from "./models/punch-info";
+export type { AuthInfo } from "./models/auth-info";
+export type { ClockType, PunchInfo } from "./models/punch-info";
 export { LoginTask } from "./tasks/login-task";
 export { LogoutTask } from "./tasks/logout-task";
 export { PunchTask } from "./tasks/punch-task";
-export { Logger, LoggerFactory } from "./utils/logger-factory";
+export type { Logger, LoggerFactory } from "./utils/logger-factory";
