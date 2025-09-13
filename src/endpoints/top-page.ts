d372751d8ea8be88f7b66de6d64ae82abfefb0f6
sengokyu <sengokyu+gh@gmail.com>
2025-09-13 15:24:15 +0900

    Refactor endpoints/login-page

--- src/endpoints/top-page.ts
+++ src/endpoints/top-page.ts
@@ -1,19 +1,9 @@
-import { AxiosRequestConfig, AxiosResponse } from "axios";
 import { BugyoCloudClient, BugyoCloudClientError } from "../bugyo-cloud-client";
 import { BASE_URL } from "../config";
 import { extractUserCode } from "../utils/url-utils";
 import { BaseEndpoint } from "./base/base-endpoint";
 
 export class TopPage extends BaseEndpoint {
-  private readonly requestConfig: AxiosRequestConfig = {
-    maxRedirects: 0,
-    // Locationが相対なので指定
-    baseURL: BASE_URL,
-    // 302のときに、エラーにならないようにする
-    validateStatus: (status) =>
-      (status >= 200 && status < 300) || status === 302,
-  };
-
   /**
    * リダイレクト後のURLからUserCodeを返します。
    *
@@ -21,7 +11,8 @@ export class TopPage extends BaseEndpoint {
    * @param url
    */
   async invoke(client: BugyoCloudClient, url: string): Promise<string> {
-    const resp = await this.wrappedHttpGet(client, url);
+    // Locationが相対なのでbaseURLを指定
+    const resp = await client.session.getAndFollow(url, { baseURL: BASE_URL });
 
     if (!resp.config.url) {
       const msg = "Cannot determin toppage url.";
@@ -32,29 +23,5 @@ export class TopPage extends BaseEndpoint {
     this.logger.info("Toppage url is %s.", resp.config.url);
 
     return extractUserCode(resp.config.url);
-  }
-
-  /**
-   * リダイレクト中の set-cookie が動かない様子なので、
-   * 自前でリダイレクトを追いかける
-   * @param client
-   * @param url
-   */
-  private async wrappedHttpGet(
-    client: BugyoCloudClient,
-    url: string
-  ): Promise<AxiosResponse> {
-    let nextUrl = url;
-    let resp: AxiosResponse;
-
-    do {
-      this.logger.debug("Trying to GET, url=%s", nextUrl);
-
-      resp = await client.session.get(nextUrl, this.requestConfig);
-    } while (resp.status === 302 && (nextUrl = resp.headers["location"]));
-
-    this.throwIfNgStatus(resp);
-
-    return resp;
   }
 }
