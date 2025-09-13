d66efdf3524d04aba48c297ba6f60718adf6a05e
sengokyu <sengokyu+gh@gmail.com>
2025-09-13 17:56:44 +0900

    Replace the factory to the service.

---
+++ src/bugyo-cloud-client-service.ts
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { HttpCookieAgent, HttpsCookieAgent } from "http-cookie-agent/http";
+import { CookieJar } from "tough-cookie";
+import { BugyoCloudClient } from "./bugyo-cloud-client";
+import { Authenticate } from "./endpoints/authenticate";
+import { BaseEndpoint } from "./endpoints/base/base-endpoint";
+import { CallLogout } from "./endpoints/call-logout";
+import { CheckAuthenticationMethod } from "./endpoints/check-authentication-method";
+import { LoginPage } from "./endpoints/login-page";
+import { OmRedirect } from "./endpoints/om-redirect";
+import { PunchmarkPage } from "./endpoints/punchmark-page";
+import { TimeClock } from "./endpoints/time-clock";
+import { AuthInfo } from "./models/auth-info";
+import { PunchInfo } from "./models/punch-info";
+import { BaseTask } from "./tasks/base/base-task";
+import { LoginTask } from "./tasks/login-task";
+import { LogoutTask } from "./tasks/logout-task";
+import { PunchTask } from "./tasks/punch-task";
+import { HttpSession } from "./utils/http-session";
+import { Logger, LoggerFactory } from "./utils/logger-factory";
+
+/**
+ * Creating a instance service
+ */
+export class BugyoCloudClientService {
+  constructor(private loggerFactory: LoggerFactory) {}
+
+  public createClient(tenantCode: string): BugyoCloudClient {
+    return new BugyoCloudClient(tenantCode, this.createHttpSession());
+  }
+
+  public createLoginTask(authInfo: AuthInfo): BaseTask {
+    return new LoginTask(
+      this.getLogger(LoginTask),
+      this.createEndpointInstance(LoginPage),
+      this.createEndpointInstance(CheckAuthenticationMethod),
+      this.createEndpointInstance(Authenticate),
+      this.createEndpointInstance(OmRedirect),
+      authInfo
+    );
+  }
+
+  public createLogoutTask(): BaseTask {
+    return new LogoutTask(
+      this.getLogger(LogoutTask),
+      this.createEndpointInstance(CallLogout)
+    );
+  }
+
+  public createPunchTask(punchInfo: PunchInfo): BaseTask {
+    return new PunchTask(
+      this.getLogger(PunchTask),
+      this.createEndpointInstance(TimeClock),
+      this.createEndpointInstance(PunchmarkPage),
+      punchInfo
+    );
+  }
+
+  private createHttpSession(): HttpSession {
+    return new HttpSession(
+      this.getLogger(HttpSession),
+      this.createAxiosInstance()
+    );
+  }
+
+  private createAxiosInstance(): axios.AxiosInstance {
+    const jar = new CookieJar();
+
+    return axios.create({
+      httpAgent: new HttpCookieAgent({ cookies: { jar } }),
+      httpsAgent: new HttpsCookieAgent({ cookies: { jar } }),
+    });
+  }
+
+  private createEndpointInstance<T extends BaseEndpoint>(
+    ctor: (new (logger: Logger) => T) & { get name(): string }
+  ): T {
+    return <T>new ctor(this.loggerFactory.getLogger(ctor.name));
+  }
+
+  private getLogger(cls: { get name(): string }): Logger {
+    return this.loggerFactory.getLogger(cls.name);
+  }
+}
